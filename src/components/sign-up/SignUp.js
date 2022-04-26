ed5d728d9e40d7af598ef0175274b47fb7e8054b
Variable131530 <hungngocct@gmail.com>
2022-04-26 22:36:34 +0700

    edit sign up page

--- src/components/sign-up/SignUp.js
+++ src/components/sign-up/SignUp.js
@@ -20,8 +20,8 @@ const SignUp = function () {
         return (
             <App>
                 <div className="App">
-                    {/* <SignUp /> */}
-                    <ConfirmOtp />
+                    <SignUp />
+                    {/* <ConfirmOtp /> */}
                 </div>
             </App>
         );
@@ -36,18 +36,16 @@ const SignUp = function () {
 
                 <div className="container">
                     <h2>Đăng ký cửa hàng</h2>
-                    <form onSubmit={handleSubmit} className="register-form">
-                        <label className="signup-label name-label" htmlFor="store-name">Tên cửa hàng*</label>
-                        <input type="text" id="store-name" placeholder='Nhập tên cửa hàng' required />
-                        <label className="signup-label phone-label" htmlFor="phone-number">Số điện thoại*</label>
-                        <input type="number" id="store-name" placeholder='Nhập số điện thoại' required />
-                        <label className="signup-label password-label" htmlFor="password">Mật khẩu*</label>
-                        <input type="password" id="store-name" placeholder='Nhập mật khẩu' required />
-                        <p>Mật khẩu phải có ít nhất 8 kí tự.</p>
-                        <label className="signup-label confirm-label" htmlFor="store-name">Xác nhận mật khẩu</label>
-                        <input type="password" id="store-name" placeholder='Xác nhận lại mật khẩu' required />
-                        <button className="signup-btn" >Đăng ký</button>
-                    </form>
+                    <label className="signup-label name-label" htmlFor="store-name">Tên cửa hàng*</label>
+                    <input type="text" id="store-name" placeholder='Nhập tên cửa hàng' required />
+                    <label className="signup-label phone-label" htmlFor="phone-number">Số điện thoại*</label>
+                    <input type="number" id="store-name" placeholder='Nhập số điện thoại' required />
+                    <label className="signup-label password-label" htmlFor="password">Mật khẩu*</label>
+                    <input type="password" id="store-name" placeholder='Nhập mật khẩu' required />
+                    <p>Mật khẩu phải có ít nhất 8 kí tự.</p>
+                    <label className="signup-label confirm-label" htmlFor="store-name">Xác nhận mật khẩu</label>
+                    <input type="password" id="store-name" placeholder='Xác nhận lại mật khẩu' required />
+                    <button className="signup-btn" >Đăng ký</button>
 
                     <div className="detail-info">
                         <span>Đã có tài khoản?</span>
